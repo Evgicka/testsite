ca2c35c54c1367ccb67d049fa6f21ae4cb004d19
Evgicka <citap.net@yandex.ru>
2020-03-02 00:43:53 +0300

    1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<form id='forma' action='get.php' method='post'> 
+<h1>Форма входа</h1> 
+<p>Заполните поля для входа на сайт</p> 
+<p>Логин<br /><input type='text' name='login'></p> 
+<p>Пароль<br /><input type='password' name='password'></p> 
+<p><input type='submit' name='submit' value='Войти'> <br>
+</p>
+</form>
+
+</body>
+</html>
